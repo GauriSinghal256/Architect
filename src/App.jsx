787550e6ed819644682b6549df432183510e5f31
GauriSinghal256 <singhalg818@gmail.com>
2025-05-31 14:52:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="services" element={<Services />} />
           <Route path="contact" element={<Contact />} />
+          
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
