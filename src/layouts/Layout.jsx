1904afbe3f40f53d33509696bfc2b40772fc69a1
Neha Garg <gargneha2409@gmail.com>
2025-07-01 17:33:56 +0530

    overflow hidden

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -17,7 +17,7 @@ const Layout = () => {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-hidden">
       <AnimatePresence>
         {isLoading ? (
           <motion.div 
